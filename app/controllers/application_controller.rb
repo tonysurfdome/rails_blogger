5a1b6972ff33af2236deea14720ac58cefd3898b
Anthony McKernan <tony@surfdome.com>
2014-02-03 16:18:30 +0000

    finished blog comments feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
